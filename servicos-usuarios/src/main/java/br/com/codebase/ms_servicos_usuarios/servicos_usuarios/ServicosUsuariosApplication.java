2b547be19bb09f73a9b19a4620e504a4b7584f9c
RAFAEL ROCO <rafael.roco.dev@gmail.com>
2025-02-05 15:04:50 -0300

    criacao da inicializacao do projeto ms-servicos-usuarios

---
+++ servicos-usuarios/src/main/java/br/com/codebase/ms_servicos_usuarios/servicos_usuarios/ServicosUsuariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.codebase.ms_servicos_usuarios.servicos_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicosUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicosUsuariosApplication.class, args);
+	}
+
+}
---
+++ servicos-usuarios/src/test/java/br/com/codebase/ms_servicos_usuarios/servicos_usuarios/ServicosUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codebase.ms_servicos_usuarios.servicos_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicosUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
