78e017fb8dd3e621786b2c870f2c18057a4bdaa3
jfsog <Felipe.sg@protonmail.com>
2025-02-12 20:23:46 -0300

    modificação da mensagem de resposta de criação

--- src/main/java/br/com/codebase/ms_servicos_usuarios/servicos_usuarios/DTO/UsuarioResponseDTO.java
+++ src/main/java/br/com/codebase/ms_servicos_usuarios/servicos_usuarios/DTO/UsuarioResponseDTO.java
@@ -1,11 +1,15 @@
 package br.com.codebase.ms_servicos_usuarios.servicos_usuarios.DTO;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class UsuarioResponseDTO {
-    String message;
-    String token;
+    private String nome;
+    private String email;
+    private String perfil;
+    private String message;
 }
