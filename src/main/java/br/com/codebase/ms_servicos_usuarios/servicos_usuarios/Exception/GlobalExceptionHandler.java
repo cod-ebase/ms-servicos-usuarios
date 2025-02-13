961ef2af46fca8b51d9eb4793b440dd31d7f14be
jfsog <Felipe.sg@protonmail.com>
2025-02-13 11:13:48 -0300

    modificações em GlobalExceptionHandler

--- src/main/java/br/com/codebase/ms_servicos_usuarios/servicos_usuarios/Exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/codebase/ms_servicos_usuarios/servicos_usuarios/Exception/GlobalExceptionHandler.java
@@ -1,22 +1,28 @@
 package br.com.codebase.ms_servicos_usuarios.servicos_usuarios.Exception;
 
-import org.springframework.dao.DataIntegrityViolationException;
+import br.com.codebase.ms_servicos_usuarios.servicos_usuarios.DTO.UsuarioResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.sql.SQLException;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<String> handleSQLExceptions(SQLException ex) {
-        String mensagem = ex.getCause() != null ? ex.getCause().getLocalizedMessage() : ex.getMessage();
-        var i = mensagem.indexOf("Detalhe:");
-        if (i > 0) {
-            mensagem = mensagem.substring(i);
-        }
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(mensagem);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult()
+                                .getFieldErrors()
+                                .stream()
+                                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                                .collect(Collectors.joining(", ", "{", "}"));
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<UsuarioResponseDTO> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
+        var b = UsuarioResponseDTO.builder().message(ex.getMessage()).build();
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(b);
     }
 }
