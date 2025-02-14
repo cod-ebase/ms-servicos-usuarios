fa1aa14d113683807eabc6ef73bf2195190e97f2
jfsog <Felipe.sg@protonmail.com>
2025-02-14 00:55:22 -0300

    modificações no controller adivice

--- src/main/java/br/com/codebase/ms_servicos_usuarios/servicos_usuarios/Exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/codebase/ms_servicos_usuarios/servicos_usuarios/Exception/GlobalExceptionHandler.java
@@ -1,28 +1,36 @@
 package br.com.codebase.ms_servicos_usuarios.servicos_usuarios.Exception;
 
-import br.com.codebase.ms_servicos_usuarios.servicos_usuarios.DTO.UsuarioResponseDTO;
+import br.com.codebase.ms_servicos_usuarios.servicos_usuarios.api.controller.UsuarioController;
+import org.modelmapper.internal.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
 import java.util.stream.Collectors;
 
-@ControllerAdvice
+@ControllerAdvice(basePackageClasses = UsuarioController.class)
 public class GlobalExceptionHandler {
+    /**
+     * Manipula exceções de validação e retorna um mapa com os campos inválidos e as suas mensagens de erro.
+     *
+     * @param ex Exceção lançada quando acontece um erro de validação nos argumentos da requisição.
+     * @return ResponseEntity contendo os campos inválidos como chave e as mensagens de erro como valor, com status
+     * 400 Bad request.
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        String errorMessage = ex.getBindingResult()
-                                .getFieldErrors()
-                                .stream()
-                                .map(error -> error.getField() + ": " + error.getDefaultMessage())
-                                .collect(Collectors.joining(", ", "{", "}"));
-        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Map<?, ?>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        var map = ex.getBindingResult()
+                    .getFieldErrors()
+                    .stream()
+                    .map(e -> Pair.of(e.getField(), e.getDefaultMessage()))
+                    .collect(Collectors.toMap(Pair::getLeft, Pair::getRight));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(map);
     }
     @ExceptionHandler(EmailAlreadyExistsException.class)
-    public ResponseEntity<UsuarioResponseDTO> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
-        var b = UsuarioResponseDTO.builder().message(ex.getMessage()).build();
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(b);
+    public ResponseEntity<Map<?, ?>> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("email", ex.getMessage()));
     }
 }
